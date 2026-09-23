a59bed6c3c4d2f4a4c5feb6b53dbb92271ecd91b
Buay Dak <buaypd@gmail.com>
2026-09-23 13:43:58 -0700

    oddSum implemented and tested

--- src/Practice.java
+++ src/Practice.java
@@ -12,7 +12,14 @@ public class Practice {
      * @return the sum of the odd numbers in the array
      */
     public static int oddSum(int[] nums) {
-        return 0;
+        if (nums == null) return 0;
+        int sum = 0;
+        for (int i : nums) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
     }
 
     /**
